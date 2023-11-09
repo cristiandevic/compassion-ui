ec28965ee8bd4739034c2058fd6303245c40aae9
cristiandevic <147736184+cristiandevic@users.noreply.github.com>
2023-11-09 10:54:34 -0400

    agregado-fuente

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+//agregando fuetes roboto
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
